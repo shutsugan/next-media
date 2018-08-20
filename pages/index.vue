b01b41d0c4c8ce2b5e955838cdd742a2687b167e
shutsugan <shutsugan@gmail.com>
2018-08-20 19:21:30 +0100

    Initialized the project, and clean it.

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<section class="container">
+		Clean ...
+	</section>
+</template>
+
+<style>
+.container {
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: center;
+	text-align: center;
+	overflow-x: hidden;
+}
+</style>
+
